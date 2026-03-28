e267545f673f3221201229dc1c3e05ce1cd0f682
Bharati-Rao <rao.bharati@gmail.com>
2026-03-28 20:38:47 +0530

    Fix reporter config

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
  reporter: [
-  ['html'],
+   ['html', { open: 'never', outputFolder: 'playwright-report' }],
   ['spec']
 ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
